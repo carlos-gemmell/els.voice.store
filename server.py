6ae3e33ce04ee02e53eb723943ae20cb24150cdf
aquaktus <aquaktus@yahoo.co.uk>
2019-08-25 20:39:05 +0200

    updating ssl

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
-from flask import Flask, request
-# set the project root directory as the static folder, you can set others.
-app = Flask(__name__, static_url_path='')
+from flask import Flask
+app = Flask(__name__)
 
-@app.route('/')
-def root():
-    return app.send_static_file('index.html')
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', 
+            port=80, 
+            ssl_context=('/etc/letsencrypt/live/elsvoicestore.org/fullchain.pem', '/etc/letsencrypt/live/elsvoicestore.org/privkey.pem'))
