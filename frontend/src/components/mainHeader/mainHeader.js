d4aab33c2770ec0c89701a707f16b479514ac336
aquaktus <aquaktus@yahoo.co.uk>
2019-08-27 13:05:41 +0200

    removing foo, adding baz

--- frontend/src/components/mainHeader/mainHeader.js
+++ frontend/src/components/mainHeader/mainHeader.js
@@ -6,7 +6,7 @@ class mainHeader extends React.Component {
 		return (
 			<div className="mainHeader" style={{"width":"100%","height":"12.5%"}}>
 				<div style={{"width":"100%","height":"2.5%"}} className="welcomeMessage">
-					Hi Dr Carlos Gemmell Gorriz foo
+					Hi Dr Carlos Gemmell Gorriz baz
 				</div>
 			</div>
 		);
