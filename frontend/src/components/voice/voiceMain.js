6a9d5de340f38765c3fd17c9af1d505209e4a0aa
palamalama <ericgemmell@gmail.com>
2019-08-27 12:48:21 +0100

    recording

--- frontend/src/components/voice/voiceMain.js
+++ frontend/src/components/voice/voiceMain.js
@@ -1,41 +1,49 @@
 import React from "react";
 import Recorder from "./recorder";
+var URL = window.URL || window.webkitURL;
+
+let AudioContext = window.AudioContext || window.webkitAudioContext;
+let rec;
+let gumStream;
 
 class mainHeader extends React.Component {
 
-	AudioContext = window.AudioContext || window.webkitAudioContext;
-
 	constructor(props){
 		super(props);
+		this.state = {
+			files: []
+		}
 		this.startRecording = this.startRecording.bind(this);
 		this.stopRecording = this.stopRecording.bind(this);
+		this.addAudioFile = this.addAudioFile.bind(this);
 	}
 	startRecording(){
-		console.log("recordButton clicked");
 		navigator.mediaDevices.getUserMedia({ audio: true, video:false }).then((stream) => {
-			let audioContext = new this.AudioContext();	
-			console.log("Audio Context",this.AudioContext)
-			console.log("audio context", audioContext);
+			let audioContext = new AudioContext();	
 			console.log("sample rate",audioContext.sampleRate);
 
-			let gumStream = stream;
+			gumStream = stream;
 			let input = audioContext.createMediaStreamSource(stream);
-			let rec = new Recorder(input,{numChannels:1})
+			rec = new Recorder(input,{numChannels:1})
 
 			//start the recording process
 			rec.record()
 
 			console.log("Recording started");
-			setTimeout(() =>{
-				rec.stop();
-				gumStream.getAudioTracks()[0].stop();
-				rec.exportWAV(createDownloadLink);
-			}, 3000);
 			
 		});
 	}
 	stopRecording(){
-
+		rec.stop();
+		gumStream.getAudioTracks()[0].stop();
+		rec.exportWAV(this.addAudioFile);
+	}
+	addAudioFile(blob){
+		let url = URL.createObjectURL(blob);
+		let filename = new Date().toISOString();
+		let newFiles = this.state.files;
+		newFiles.push({url:url,filename:filename});
+		this.setState({files:newFiles});
 	}
 	render() {
 		console.log(this.state);
@@ -44,6 +52,7 @@ class mainHeader extends React.Component {
 				VOICE SECTION!!!!
 				<button onClick={this.startRecording} type="button">Start</button>
 				<button onClick={this.stopRecording} type="button">Stop</button>
+				{this.state.files.map((file) =>(<audio autoplay="true" controls="true" src={file.url}/>))}
 			</div>
 	       );
 	}
@@ -51,5 +60,6 @@ class mainHeader extends React.Component {
 export default mainHeader;
 
 function createDownloadLink(blob) {
+	console.log(blob);
 	Recorder.forceDownload(blob)
 }
