1c7ab92dbdb0426c1994294d712b2678651e0640
aquaktus <aquaktus@yahoo.co.uk>
2019-08-28 01:15:07 +0200

    login page style changes to center

--- frontend/src/components/login/loginMain.js
+++ frontend/src/components/login/loginMain.js
@@ -1,4 +1,5 @@
 import React from "react";
+import logo from '../../images/logo.png';
 
 class Login extends React.Component {
 
@@ -27,23 +28,32 @@ class Login extends React.Component {
 			
 	}
 	render() {
+		let input_style = {
+			"padding": "12px 20px",
+			"margin": "8px 0",
+			"display": "inline-block",
+			"border": "1px solid #ccc",
+			"border-radius": "4px",
+			"box-sizing": "border-box"
+		}
 		return (
 			<div className="LoginPageMain" style={{"height":"100%","width":"100%"}}>
-				<div className="LoginHeader" style={{"width":"100%","height":"20%","fontSize":"5vw","textAlign":"center"}}>
+				{/* <div className="LoginHeader" style={{"width":"100%","height":"20%","fontSize":"5vw","textAlign":"center"}}>
 					Log into ELS Voice Store
-				</div>
-				<div className="LoginForm" style={{"width":"100%","height":"80%"}}>
+				</div> */}
+				<img src={logo} style={{"width":"50%", "height":"100%", "left":"25%", "display":"block", "objectFit":"contain", "float":"center", "position":"absolute"}}/>
+				<div className="LoginForm" style={{"text-align":"center", "top":"20%", "width":"100%","height":"80%", "position":"absolute"}}>
 					<div className="usernameInput" style={{}}>
 						<div className="usernameLabel">
 							Username
 						</div>
-						<input ref={(username) => this.username = username} type="text" placeholder="Enter Username" name="username"/>
+						<input ref={(username) => this.username = username} style={input_style} type="text" placeholder="Enter Username" name="username"/>
 					</div>
 					<div className="passwordInput" style={{}}>
 						<div className="passwordLabel">
 							Password
 						</div>
-						<input ref={(password) => this.password = password} type="password" placeholder="Enter Password" name="password"/>
+						<input ref={(password) => this.password = password} style={input_style} type="password" placeholder="Enter Password" name="password"/>
 					</div>
 					<button onClick={this.loginButtonClick} >Login</button>
 				</div>
