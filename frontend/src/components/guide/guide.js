f15f8a7639838e90faf6948843968af9586cbcbc
aquaktus <aquaktus@yahoo.co.uk>
2019-09-06 13:00:34 +0200

    changing animation time to be more respponsive

--- frontend/src/components/guide/guide.js
+++ frontend/src/components/guide/guide.js
@@ -5,7 +5,8 @@ class guide extends React.Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			"max-height":"15%"
+			"max-height":"15%",
+			"transition-time":"3000ms"
 		}
 		this.toggle_height = this.toggle_height.bind(this)
 	}
@@ -14,16 +15,17 @@ class guide extends React.Component {
 	toggle_height(){
 		if (this.state["max-height"] == "15%"){
 			this.setState({"max-height":"600%"});
+			this.setState({"transition-time":"3000ms"});
 		}
 		else{
 			this.setState({"max-height":"15%"});
+			this.setState({"transition-time":"500ms"});
 		}
 	}
 	
 	render() {
 		return (
-			<div className={"guide"} style={{"transition": "max-height 3000ms ease-in-out", "overflow":"hidden", "width":"98%","height":"auto", "max-height":this.state["max-height"], "border-style":"solid","border-color": "white", "margin":"0.5%"}}>
-				<button style={{"font-size": "2vh"}} onClick={this.toggle_height} >Expand</button>
+			<div className={"guide"} style={{"transition": "max-height " + this.state['transition-time'] + " ease-in-out", "overflow":"hidden", "width":"98%","height":"auto", "max-height":this.state["max-height"], "border-style":"solid","border-color": "white", "margin":"0.5%"}} onClick={this.toggle_height}>
 				Guide to use the application:<br />
 				1 - do something <br />
 				2 - do something else <br />
