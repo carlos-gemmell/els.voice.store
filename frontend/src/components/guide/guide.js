c43bc2804f3bbb55847e8a87a592f144e8936d3d
sweatysock <mark@electricgt.co>
2019-09-16 18:06:24 +0200

    Tidying up the text

--- frontend/src/components/guide/guide.js
+++ frontend/src/components/guide/guide.js
@@ -33,25 +33,25 @@ class guide extends React.Component {
 				Store anonymous patient voice recordings for future analysis.<br />
 				<br />
 				Place the microphone 10cms from the patient and at 45 degrees to the side in order
-				to avoid breath noise.<br />
+				to avoid breath noise.<br /><br />
 				1 - Press the microphone icon to start recording. <br />
 				Explain to the patient that their voice will be recorded and stored anonymously 
 				in the ELS cloud for future analysis. 
-				Ask if they agree to their voice being stored there. <br />
+				Ask if they agree to their voice being stored there. <br /><br />
 				2 - Stop recording by pressing again the microphone icon and if the patient agreed 
-				then press the cloud icon to store the file.<br />
+				then press the cloud icon to store the file.<br /><br />
 				3 - If the patient did not agree you can still use the app to analyse their voice, 
-				but do not upload any files.<br />
+				but do not upload any files.<br /><br />
 				4 - Record the patient saying "Ahhhh" for as long as possible. 
-				Keep background noise low.<br />
+				Keep background noise low.<br /><br />
 				5 - Note the CPPs value displayed on the screeen in the patient's medical 
-				record for this recording and upload this to the cloud<br />
+				record for this recording and upload this to the cloud<br /><br />
 				6 - Record the patient repeating a sentence with no fricatives (without sounds "s", "f" "z" etc.) such as:<br />
 					"Eat, drink and be merry" or "hard work never did anyone any harm".<br />
-				Again note the CPPs for this recording and upload it to the cloud.
-				7 - Optional: Record the patient describing their voice problems<br />
-				Note the CPPs value here to if desired and upload to the cloud too.
-				<br />
+				Again note the CPPs for this recording and upload it to the cloud.<br /><br />
+				7 - Optional: Record the patient describing their voice problems.<br />
+				Note the CPPs value here too if desired and upload to the cloud as well.
+				<br /><br />
 				Note that files are only visbile to you and the ELS Voice Store administrators and
 				contain no patient specific information.<br />
 				Files are stored and ordered by date and time of recording. 
