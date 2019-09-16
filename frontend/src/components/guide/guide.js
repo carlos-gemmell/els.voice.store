d8ef9332353812319ef4d420b70e799492514798
sweatysock <mark@electricgt.co>
2019-09-16 16:40:39 +0200

    Guide review

--- frontend/src/components/guide/guide.js
+++ frontend/src/components/guide/guide.js
@@ -33,20 +33,30 @@ class guide extends React.Component {
 				Store anonymous patient voice recordings for future analysis.<br>
 				<p>
 				Record the patient placing the microphone 10cms from their mouth 45 degrees to the side.<br>
-				1 - Press the microphone icon to start recording. 
-					Explain to the patient that their voice will be recorded and stored anonymously 
-					in the ELS cloud for future analysis. Ask if they agree to allow you to record ands tore their voice.<br>
-				2 - Stop recording by pressing again the microphone icon and if the patient agreed then press the cloud icon to store the file.<br />
-				3 - If the patient did not agree you can still use the app to analyse their voice, but no files should be uploaded.<br>
-				4 - Record the patient saying "Ahhhh" for as long as possible. watch out for background noise.<br>
-				5 - Note the CPPs value in the patient's record for this recording <br>
+				1 - Press the microphone icon to start recording. <br>
+				Explain to the patient that their voice will be recorded and stored anonymously 
+				in the ELS cloud for future analysis. 
+				Ask if they agree to their voice being stored there. <br>
+				2 - Stop recording by pressing again the microphone icon and if the patient agreed 
+				then press the cloud icon to store the file.<br />
+				3 - If the patient did not agree you can still use the app to analyse their voice, 
+				but no files should be uploaded.<br>
+				4 - Record the patient saying "Ahhhh" for as long as possible. 
+				Keep background noise low.<br>
+				5 - Note the CPPs value displayed on the screeen in the patient's medical 
+				record for this recording <br>
 				6 - Record the patient repeating a sentence with no fricatives (without sounds "s", "f" "z" etc.) such as:<br>
 					"Eat, drink and be merry" or "hard work never did anyone any harm".<br>
+				Again note the CPPs for this recording.
 				7 - Optional: Record the patient describing their voice problems<br>
-				Note that files are only visbile to your user and the ELS Voice Store administrators. 
+				Note the CPPs value here to if desired.
+				<p>
+				Note that files are only visbile to you and the ELS Voice Store administrators and
+				they contain no patient specific information.<br>
 				Files are stored and ordered by date and time of recording. 
-				You can review patient files by looking for the recordings corresponding to the date and time of the specific appointment.
-				
+				You can review patient files by looking for the recordings corresponding to 
+				the date and time of the petient appointment.
+
 			</div>
 		);
 	}
