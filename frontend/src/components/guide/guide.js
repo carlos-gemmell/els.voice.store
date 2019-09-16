ac2d4e96cd4bf0fd0a3627e3a186975e96407b25
sweatysock <mark@electricgt.co>
2019-09-16 17:53:48 +0200

    trying h2 and new text

--- frontend/src/components/guide/guide.js
+++ frontend/src/components/guide/guide.js
@@ -29,10 +29,11 @@ class guide extends React.Component {
 				"color":"white"}} 
 				onClick={this.toggle_height}
 			>
-				ELS Voice Store Application<br />
+				<h2>ELS Voice Store Application</h2>
 				Store anonymous patient voice recordings for future analysis.<br />
 				<br />
-				Record the patient placing the microphone 10cms from their mouth 45 degrees to the side.<br />
+				Place the microphone 10cms from the patient and at 45 degrees to the side in order
+				to avoid breath noise.<br />
 				1 - Press the microphone icon to start recording. <br />
 				Explain to the patient that their voice will be recorded and stored anonymously 
 				in the ELS cloud for future analysis. 
@@ -40,22 +41,22 @@ class guide extends React.Component {
 				2 - Stop recording by pressing again the microphone icon and if the patient agreed 
 				then press the cloud icon to store the file.<br />
 				3 - If the patient did not agree you can still use the app to analyse their voice, 
-				but no files should be uploaded.<br />
+				but do not upload any files.<br />
 				4 - Record the patient saying "Ahhhh" for as long as possible. 
 				Keep background noise low.<br />
 				5 - Note the CPPs value displayed on the screeen in the patient's medical 
-				record for this recording <br />
+				record for this recording and upload this to the cloud<br />
 				6 - Record the patient repeating a sentence with no fricatives (without sounds "s", "f" "z" etc.) such as:<br />
 					"Eat, drink and be merry" or "hard work never did anyone any harm".<br />
-				Again note the CPPs for this recording.
+				Again note the CPPs for this recording and upload it to the cloud.
 				7 - Optional: Record the patient describing their voice problems<br />
-				Note the CPPs value here to if desired.
+				Note the CPPs value here to if desired and upload to the cloud too.
 				<br />
 				Note that files are only visbile to you and the ELS Voice Store administrators and
-				they contain no patient specific information.<br />
+				contain no patient specific information.<br />
 				Files are stored and ordered by date and time of recording. 
-				You can review patient files by looking for the recordings corresponding to 
-				the date and time of the petient appointment.
+				You can review previous recordings by looking in the file list below for the 
+				recordings corresponding to the date and time of the patient's appointment.
 
 			</div>
 		);
