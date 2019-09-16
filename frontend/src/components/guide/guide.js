94f0cc57084e1961d2508921e23a684226c7bc8b
sweatysock <mark@electricgt.co>
2019-09-16 18:51:46 +0200

    Text adjustments

--- frontend/src/components/guide/guide.js
+++ frontend/src/components/guide/guide.js
@@ -33,26 +33,24 @@ class guide extends React.Component {
 				Store anonymous patient voice recordings for future analysis.<br />
 				<br />
 				Place the microphone 10cms from the patient and at 45 degrees to the side in order
-				to avoid breath noise.<br /><br />
+				to avoid breath noise. Keep background noise low.<br /><br />
 				1 - Press the microphone icon to start recording. <br />
 				Explain to the patient that their voice will be recorded and stored anonymously 
 				in the ELS cloud for future analysis. 
-				Ask if they agree to their voice being stored there. <br /><br />
-				2 - Stop recording by pressing again the microphone icon and if the patient agreed 
-				then press the cloud icon to store the file.<br /><br />
-				3 - If the patient did not agree you can still use the app to analyse their voice, 
-				but do not upload any files.<br /><br />
-				4 - Record the patient saying "Ahhhh" for as long as possible. 
-				Keep background noise low.<br /><br />
-				5 - Note the CPPs value displayed on the screeen in the patient's medical 
-				record for this recording and upload this to the cloud<br /><br />
-				6 - Record the patient repeating a sentence with no fricatives 
+				Confrim they agree to these recordings being stored there. <br /><br />
+				2 - Stop recording by pressing again the microphone icon and, assuming the patient 
+				agreed, press the cloud icon to store this recording giving consent 
+				(required for legal purposes).<br /><br />
+				3 - Record the patient saying "Ahhhh" for as long as possible.  <br /><br />
+				4 - Note the CPPs value displayed on the screeen in the patient's medical 
+				record for this recording and upload it to the cloud<br /><br />
+				5 - Record the patient repeating a sentence with no fricatives 
 				(without sounds "s", "f" "z" etc.) such as:<br />
-					<b>"Eat, drink and be merry"</b> or 
+					<b>"Eat, drink and be merry"</b> or <br />
 					<b>"hard work never did anyone any harm"</b>.<br />
 				Again note the CPPs for this recording and upload it to the cloud.<br /><br />
-				7 - Optional: Record the patient describing their voice problems.<br />
-				Note the CPPs value here too if desired and upload to the cloud as well.
+				6 - Optional: Record the patient describing their voice problems.<br />
+				Note the CPPs value here too if desired and upload to the cloud.
 				<br /><br />
 				<b>Note that files are only visbile to you and the ELS Voice Store administrators and
 				contain no patient specific information.</b><br />
