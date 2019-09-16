d4bacd1e8ce6186d237a69e110b0455be786f18d
sweatysock <mark@electricgt.co>
2019-09-16 18:12:12 +0200

    Trying bold again

--- frontend/src/components/guide/guide.js
+++ frontend/src/components/guide/guide.js
@@ -29,7 +29,7 @@ class guide extends React.Component {
 				"color":"white"}} 
 				onClick={this.toggle_height}
 			>
-				<h2>ELS Voice Store Application</h2>
+				<b>ELS Voice Store Application<b />
 				Store anonymous patient voice recordings for future analysis.<br />
 				<br />
 				Place the microphone 10cms from the patient and at 45 degrees to the side in order
