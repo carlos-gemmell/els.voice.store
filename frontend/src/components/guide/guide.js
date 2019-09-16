9cc579df99ca23d9a3a5cb388ab7bec8f53287aa
sweatysock <53512142+sweatysock@users.noreply.github.com>
2019-09-16 16:12:06 +0200

    First complete guide version

--- frontend/src/components/guide/guide.js
+++ frontend/src/components/guide/guide.js
@@ -29,35 +29,24 @@ class guide extends React.Component {
 				"color":"white"}} 
 				onClick={this.toggle_height}
 			>
-				<b>ELS Voice Store Application</b><br>
+				<h1>ELS Voice Store Application</h1>
 				Store anonymous patient voice recordings for future analysis.<br>
-				Guide to use the application:<br>
-				1 - Explain to the patient that their voice will be recorded and stored anonymously in the ELS cloud for future analysis.<br />
-				2 - If they agre then record them saying<br />
-				3 - last thing to do because its simple!
-				foo <br/>
-				foo <br/>
-				foo <br/>
-				foo <br/>
-				foo <br/>
-				foo <br/>
-				foo <br/>
-				foo <br/>
-				foo <br/>
-				foo <br/>
-				foo <br/>
-				foo <br/>
-				foo <br/>
-				foo <br/>
-				foo <br/>
-				foo <br/>
-				foo <br/>
-				foo <br/>
-				foo <br/>
-				foo <br/>
-				foo <br/>
-				foo <br/>
-				foo <br/>
+				<p>
+				Record the patient placing the microphone 10cms from their mouth 45 degrees to the side.<br>
+				1 - Press the microphone icon to start recording. 
+					Explain to the patient that their voice will be recorded and stored anonymously 
+					in the ELS cloud for future analysis. Ask if they agree to allow you to record ands tore their voice.<br>
+				2 - Stop recording by pressing again the microphone icon and if the patient agreed then press the cloud icon to store the file.<br />
+				3 - If the patient did not agree you can still use the app to analyse their voice, but no files should be uploaded.<br>
+				4 - Record the patient saying "Ahhhh" for as long as possible. watch out for background noise.<br>
+				5 - Note the CPPs value in the patient's record for this recording <br>
+				6 - Record the patient repeating a sentence with no fricatives (without sounds "s", "f" "z" etc.) such as:<br>
+					"Eat, drink and be merry" or "hard work never did anyone any harm".<br>
+				7 - Optional: Record the patient describing their voice problems<br>
+				Note that files are only visbile to your user and the ELS Voice Store administrators. 
+				Files are stored and ordered by date and time of recording. 
+				You can review patient files by looking for the recordings corresponding to the date and time of the specific appointment.
+				
 			</div>
 		);
 	}
